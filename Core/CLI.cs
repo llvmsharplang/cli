c6b4c7a9e315546d01a7474e39d8431b49817ccc
Draem <23418831+Draem@users.noreply.github.com>
2019-04-26 12:10:36 -0400

    Initial commit

---
+++ Core/CLI.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+
+
+namespace cli
+{
+
+    public class FileResult
+    {
+        public string path { get; }
+        public byte[] content { get; }
+
+        public int Length { get => this.content.Length; }
+
+        public FileResult(string path)
+        {
+            this.path = path;
+            this.content = File.ReadAllBytes(this.path);
+        }
+
+        public override string ToString()
+        {
+            return System.Text.Encoding.UTF8.GetString(this.content);
+        }
+    }
+
+    public class CLI
+    {
+        public const string extension = ".cs";
+
+        public string path;
+        public bool relative;
+        public FileResult[] files;
+
+        public CLI(string path)
+        {
+            // The path is relative if it starts with './' or '../'.
+            this.relative = path.StartsWith(".");
+
+            this.path = this.relative ? // If it's relative.
+                        Path.Combine(Directory.GetCurrentDirectory(), path) : // Combine the current directory and it.
+                        path; // Else, use the path.
+        }
+
+        public CLI Scan()
+        {
+            // Get the files, using AllDirectories search option to search subfolders.
+            string[] filePaths = Directory.GetFiles(this.path, $"*{extension}", SearchOption.AllDirectories);
+            this.files = new FileResult[filePaths.Length];
+
+            // Make file results out of the paths.
+            for (var i = 0; i < filePaths.Length; i++)
+            {
+                this.files[i] = new FileResult(filePaths[i]);
+            }
+
+            return this;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System;
+
+namespace cli
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CLI cli = new CLI(args.Length > 0 ? args[0] : Directory.GetCurrentDirectory());
+            cli.Scan();
+            Console.WriteLine($"Paths: ({cli.files.Length})");
+            for (var i = 0; i < cli.files.Length; i++)
+            {
+                Console.WriteLine($"Path {i + 1}: {cli.files[i].path} | {cli.files[i].Length} bytes");
+            }
+        }
+    }
+}
