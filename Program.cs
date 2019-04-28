cd124aa524c008890fb127e7468287c8e9d8ff99
CloudRex <cloudrex@outlook.com>
2019-04-28 12:55:17 -0400

    Add CLI parser package

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System;
 using LlvmSharpLang.CLI.Core;
+using LlvmSharpLang;
 
 namespace LlvmSharpLang.CLI
 {
@@ -8,15 +9,7 @@ namespace LlvmSharpLang.CLI
     {
         public static void Main(string[] args)
         {
-            ConsoleInterface cli = new ConsoleInterface(args.Length > 0 ? args[0] : Directory.GetCurrentDirectory());
-
-            cli.Scan();
-            Console.WriteLine($"Paths: ({cli.files.Length})");
-
-            for (var i = 0; i < cli.files.Length; i++)
-            {
-                Console.WriteLine($"Path {i + 1}: {cli.files[i].Path} | {cli.files[i].Length} bytes");
-            }
+            
         }
     }
 }
