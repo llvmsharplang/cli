e1c08be0c2c34f290e711155965e2bd8487929bd
CloudRex <cloudrex@outlook.com>
2019-05-11 16:50:28 -0400

    Implement Verify method

--- PackageManagement/DependencySource.cs
+++ PackageManagement/DependencySource.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 
 namespace IonCLI.PackageManagement
 {
@@ -26,10 +27,39 @@ namespace IonCLI.PackageManagement
 
         public abstract string URL { get; }
 
+        /// <summary>
+        /// Verifies that the remote URL is up and reachable
+        /// by sending a web request without downloading any
+        /// data.
+        /// </summary>
         public virtual bool Verify()
         {
-            // TODO: Implement.
-            throw new NotImplementedException();
+            try
+            {
+                // Create the web request instance.
+                HttpWebRequest request = HttpWebRequest.Create(this.URL) as HttpWebRequest;
+
+                // Specify to only retrieve headers, avoid downloads.
+                request.Method = "HEAD";
+
+                // Set the maximum time to wait until timeout occurs.
+                request.Timeout = 5000;
+
+                // Invoke the request and retrieve the response.
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+                // Retrieve the response's status code.
+                int statusCode = (int)response.StatusCode;
+
+                // Return whether the status code indicates success.
+                return statusCode > 100 && statusCode < 400;
+            }
+            // TODO: What about catching normal Exceptions? Will this render an unhandled exception error?
+            catch (WebException)
+            {
+                // Upon any web exception, return false.
+                return false;
+            }
         }
     }
 }
