0b0c1e96cc3bd6a18ee69a65de873f50542ec022
RONTheCookie <me@ronthecookie.me>
2019-05-11 22:07:32 +0300

    Use explicit parameters

--- .scripts/linux/package.sh
+++ .scripts/linux/package.sh
@@ -23,7 +23,7 @@ cp $INSTALLERS_PATH/*.txt $PUBLISH_PATH
 cd $PUBLISH_PATH
 
 # Zip publish directory.
-tar czf $ROOT/$PACKAGES_PATH/ion-cli-linux-x64-v0.0.0.tar.gz .
+tar -czf $ROOT/$PACKAGES_PATH/ion-cli-linux-x64-v0.0.0.tar.gz .
 
 # Inform operation completed.
 echo "Operation completed"
