7091946cfac9a9b93075b76ec9b2474059163817
CloudRex <cloudrex@outlook.com>
2019-05-13 21:40:14 -0400

    Add reference to main project

--- IonCLI.Tests/ActivePathTests.cs
+++ IonCLI.Tests/ActivePathTests.cs
@@ -6,22 +6,21 @@ namespace IonCLI.Tests
     [TestFixture]
     internal sealed class ActivePathTests
     {
+        public const string root = "root";
+
         [Test]
-        [TestCase("test", "root/test")]
-        [TestCase("test.ext", "root/test.ext")]
+        [TestCase("test", @"root\test")]
+        [TestCase("test.ext", @"root\test.ext")]
         public void Resolve(string input, string expected)
         {
-            // Create the root path.
-            string root = "root";
-
             // Create the active path instance.
-            ActivePath activePath = new ActivePath(root);
+            ActivePath activePath = new ActivePath(ActivePathTests.root);
 
             // Resolve the input path.
             string actual = activePath.Resolve(input);
 
             // Compare result.
-            Assert.AreEqual(expected, input);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
