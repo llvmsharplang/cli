4bf7b48841a8d4ddb83c4b99062be27a4cdfe5a4
CloudRex <cloudrex@outlook.com>
2019-05-18 18:43:43 -0400

    Set version string as environment variable in the dev. environment setup script

--- .scripts/windows/setup-env.bat
+++ .scripts/windows/setup-env.bat
@@ -2,5 +2,8 @@
 
 echo Setting up development environment ...
 
+REM Setup environment variables used by other scripts.
+SET VERSION="0.0.3-alpha"
+
 REM Install tools.
 Powershell.exe -ExecutionPolicy remotesigned -File .\install-tools.ps1
