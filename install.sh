b090d651a276ad24cd289475d64a46d0a864bede
CloudRex <cloudrex@outlook.com>
2019-06-07 23:58:58 -0400

    Minor improvements for the Linux quick-install script

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
 # This is a conviniency bash installer script, designed to be invoked remotely by curl.
 # -------------------------------------------------------------------------------------
 
